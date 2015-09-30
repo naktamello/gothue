c32125b48e20f06479ebe311deba622f71a063d6
naktamello <joejsy@gmail.com>
2015-10-01 00:10:44 +0900

    cleaned up pepper_det.py

--- color_filters.py
+++ color_filters.py
@@ -1,24 +1,39 @@
 #!/usr/bin/python2.7
 import cv2
 import numpy as np
-filter_red = {'lower':{'lowb' : [0,200,0], 'upb' : [19,255,255]}, 'upper':{'lowb' : [160,100,50], 'upb' : [179,255,255]}, 'weight': 0.5}
-filter_blue = {'lower':{'lowb' : [5,100,100], 'upb' : [10,255,255]}, 'upper':{'lowb' : [90,100,100], 'upb' : [137,255,255]}, 'weight': 0.0}
+filter_red = {'tone0':{'lowb' : [0,200,0], 'upb' : [19,255,255]}, 'tone1':{'lowb' : [160,100,50], 'upb' : [179,255,255]}, 'weight': 0.5}
+filter_blue = {'tone0':{'lowb' : [5,100,100], 'upb' : [10,255,255]}, 'tone1':{'lowb' : [90,100,100], 'upb' : [137,255,255]}, 'weight': 0.0}
 
 
 class ColorFilter:
     def __init__(self, color, filter):
         self.color = color
-        self.filter_settings = filter
+        self.settings = filter
+        print ("filter preset = " + str(self.settings))
 
+    def open_image(self, thresh):
+        #create structuring element that will be used to "dilate" and "erode" image.
+        #the element chosen here is a 3px by 3px rectangle
+        erodeElement = cv2.getStructuringElement( cv2.MORPH_ELLIPSE,(3,3))
+        #dilate with larger element so make sure object is nicely visible
+        dilateElement = cv2.getStructuringElement( cv2.MORPH_ELLIPSE,(8,8))
+        thresh = cv2.erode(thresh,erodeElement,2)
+        thresh = cv2.dilate(thresh,dilateElement,2)
+    
+        return thresh
 
-def color_filter_init():
-    global red
-    red = ColorFilter('red', filter_red)
-    print ("red filter preset = " + str(red.filter_settings))
-    global blue
-    blue= ColorFilter('blue', filter_blue)
-    print ("blue filter preset = " + str(blue.filter_settings))
-    print("initializing color_filters")
+    def apply_filter(self, frame):
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        #add color ranges as a weighted sum (tones of the same color) (i.e. crimson vs slight orange tint)
+        tone0 = cv2.inRange(hsv, np.array(self.settings['tone0']['lowb']), np.array(self.settings['tone0']['upb']))
+        tone1 = cv2.inRange(hsv, np.array(self.settings['tone1']['lowb']), np.array(self.settings['tone1']['upb']))
+        threshold = cv2.addWeighted(tone0, float(self.settings['weight']), tone1, float(1.0-self.settings['weight']), 0.0)
+        threshold = cv2.GaussianBlur(threshold, (5,5), 0 )
+        #morphoOps erodes & dilates sequentially to "open" the previously filtered hsv image
+        threshold = self.open_image(threshold)
+        threshold = cv2.threshold(threshold, 127, 255, cv2.THRESH_BINARY)[1]
+        return hsv, threshold
 
 def open_trackbar():
     global list_of_filters
@@ -30,24 +45,24 @@ def open_trackbar():
         this_filter = list_of_filters[i]
         cv2.namedWindow(str(this_filter.color), cv2.WINDOW_AUTOSIZE)
         cv2.resizeWindow(str(this_filter.color), 480,360)
-        for x in range (0, len(np.array(this_filter.filter_settings['lower']['lowb']))):
+        for x in range (0, len(np.array(this_filter.settings['tone0']['lowb']))):
             trackbar_name = 'lower_lowb' + str(x)
-            this_array = np.array(this_filter.filter_settings['lower']['lowb'])
+            this_array = np.array(this_filter.settings['tone0']['lowb'])
             cv2.createTrackbar(trackbar_name, str(this_filter.color), 0, 255, trackbar_callback)
             cv2.setTrackbarPos(trackbar_name, str(this_filter.color), this_array[x])
-        for x in range (0, len(np.array(this_filter.filter_settings['lower']['upb']))):
+        for x in range (0, len(np.array(this_filter.settings['tone0']['upb']))):
             trackbar_name = 'lower_upb' + str(x)
-            this_array = np.array(this_filter.filter_settings['lower']['upb'])
+            this_array = np.array(this_filter.settings['tone0']['upb'])
             cv2.createTrackbar(trackbar_name, str(this_filter.color), 0, 255, trackbar_callback)
             cv2.setTrackbarPos(trackbar_name, str(this_filter.color), this_array[x])
-        for x in range (0, len(np.array(this_filter.filter_settings['upper']['lowb']))):
+        for x in range (0, len(np.array(this_filter.settings['tone1']['lowb']))):
             trackbar_name = 'upper_lowb' + str(x)
-            this_array = np.array(this_filter.filter_settings['upper']['lowb'])
+            this_array = np.array(this_filter.settings['tone1']['lowb'])
             cv2.createTrackbar(trackbar_name, str(this_filter.color), 0, 255, trackbar_callback)
             cv2.setTrackbarPos(trackbar_name, str(this_filter.color), this_array[x])
-        for x in range (0, len(np.array(this_filter.filter_settings['upper']['upb']))):
+        for x in range (0, len(np.array(this_filter.settings['tone1']['upb']))):
             trackbar_name = 'upper_upb' + str(x)
-            this_array = np.array(this_filter.filter_settings['upper']['upb'])
+            this_array = np.array(this_filter.settings['tone1']['upb'])
             cv2.createTrackbar(trackbar_name, str(this_filter.color), 0, 255, trackbar_callback)
             cv2.setTrackbarPos(trackbar_name, str(this_filter.color), this_array[x])
 
--- pepper_det.py
+++ pepper_det.py
@@ -11,59 +11,86 @@ import numpy as np
 import cv2
 
 #user files
-import color_filters
+import color_filters as FT
 import defines as DEF
 
+#globals
 feature_params = dict( maxCorners = 100, qualityLevel = 0.3, minDistance = 7, blockSize = 7)
 lk_params = dict( winSize = (15, 15), maxLevel = 2, criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
+
 class App:
-    def __init__(self, video_src):
+    def __init__(self, name, video_src):
+        #common
+        self.name = name
+        global capture
+        capture = cv2.VideoCapture(video_src)
+        self.cam = capture
+        global frame
+        _, frame = capture.read()
+        global canvas, edges
+        canvas = np.zeros_like(frame)
+        edges = np.zeros_like(frame)
+        global frame_gray
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        self.frame_idx = 0
+        #camera movement tracking
         self.track_len = 10
         self.detect_interval = 5
-        self.tracks = []
-        self.cam = cap
-        self.frame_idx = 0
 
-    def run(self, frame, frame_to_draw, threshold):
-            frames = []
-            lines = None
-            frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            frame_copy = frame_to_draw.copy()
-            frame_gray = cv2.equalizeHist(frame_gray)
-            edges = cv2.Canny(frame_gray,10,250,apertureSize = 3)
-            erodeElement = cv2.getStructuringElement( cv2.MORPH_ELLIPSE,(3,3))
-            dilateElement = cv2.getStructuringElement( cv2.MORPH_ELLIPSE,(3,3))
-            #edges = cv2.erode(edges,erodeElement,2)
-            edges = cv2.dilate(edges,dilateElement,1)
-            #edges = cv2.GaussianBlur(edges, (13,13), 0)
-            #lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength=30,maxLineGap=5)
-            if lines is not None:
-                for line in lines:
-                    x1,y1,x2,y2 = line[0]
-                    cv2.line(frame_to_draw,(x1,y1),(x2,y2),DEF.BLUE,2)
-            edges = cv2.bitwise_not(edges)
-            edges = cv2.adaptiveThreshold(edges,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,5,5)
-            #edges = cv2.threshold(edges, 150, 200, cv2.THRESH_TRUNC)
-            edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
+    def next_frame(self):
+        global frame, frame_gray, canvas
+        _, frame = capture.read()
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        canvas = np.zeros_like(frame)
 
+    def video_status(self):
+        return capture.isOpened()
+        
+    def __find_lines(self, edges):
+        global canvas
+        lines = None
+        lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength=30,maxLineGap=5)
+        if lines is not None:
+            for line in lines:
+                x1,y1,x2,y2 = line[0]
+                cv2.line(canvas,(x1,y1),(x2,y2),DEF.BLUE,2)
 
-            frame_copy = cv2.addWeighted(frame_copy, 0.5, edges, 0.5, 0.0)
-
+    def __find_edges(self, frame):
+        global frame_gray
+        equilized = np.zeros_like(frame_gray)
+        cv2.equalizeHist(frame_gray, equilized)
+        edges = cv2.Canny(equilized,10,250,apertureSize = 3)
+        dilate_element = cv2.getStructuringElement( cv2.MORPH_ELLIPSE,(3,3))
+        edges = cv2.dilate(edges,dilate_element,1)
+        #edges = cv2.GaussianBlur(edges, (13,13), 0)
+        edges = cv2.bitwise_not(edges)
+        edges = cv2.adaptiveThreshold(edges,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,5,5)
+        #edges = cv2.threshold(edges, 150, 200, cv2.THRESH_TRUNC)
+        edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
+        return edges
+    
+    class __camera_tracker:
+        def __init__(self, frame, prev_gray):
+            self.tracks = []
+            self.frame = frame
+            self.prev_gray = prev_gray
+            
+        def track_features(self):
+            global canvas
             if len(self.tracks) > 0:
-                img0, img1 = self.prev_gray, frame_gray
+                gray0, gray1 = self.prev_gray, cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
                 d_x_mean = 0
                 d_y_mean = 0
-                p0 = np.float32([tr[-1] for tr in self.tracks]).reshape(-1, 1, 2)
-                p1, st, err = cv2.calcOpticalFlowPyrLK(img0, img1, p0, None, **lk_params)
-                p0r, st, err = cv2.calcOpticalFlowPyrLK(img1, img0, p1, None, **lk_params)
-                d = abs(p0-p0r).reshape(-1, 2).max(-1)
+                p0 = np.float32([track[-1] for track in self.tracks]).reshape(-1, 1, 2)
+                p1, st, err = cv2.calcOpticalFlowPyrLK(gray0, gray1, p0, None, **lk_params)
+                p0r, st, err = cv2.calcOpticalFlowPyrLK(gray1, gray0, p1, None, **lk_params)
+                d_abs= abs(p0-p0r).reshape(-1, 2).max(-1)
                 d_raw = (p0-p0r).reshape(-1,2).max(-1)
                 if (len(d_raw) > 1):
                     d_x_mean = d_raw[0].mean()
                     d_y_mean = d_raw[1].mean()
-                good = d < 1
+                good = d_abs < 1
                 new_tracks = []
                 for tr, (x, y), good_flag in zip(self.tracks, p1.reshape(-1, 2), good):
                     if not good_flag:
@@ -72,21 +99,20 @@ class App:
                     if len(tr) > self.track_len:
                         del tr[0]
                     new_tracks.append(tr)
-                    #cv2.circle(frame_to_draw, (x, y), 2, DEF.GREEN, -1)
+                    #cv2.circle(canvas, (x, y), 2, DEF.GREEN, -1)
                 self.tracks = new_tracks
-                #cv2.line(frame_to_draw,(x,y), tr[len(tr)-1], DEF.BLUE)
+                #cv2.line(canvas,(x,y), tr[len(tr)-1], DEF.BLUE)
                 #cv2.poly
-                #cv2.polylines(frame_to_draw, [np.int32(tr) for tr in self.tracks], False, DEF.GREEN)
-                frame_to_draw = cv2.addWeighted(frame_to_draw, 0.3, frame_copy, 0.7, 0.0)
+                #cv2.polylines(canvas, [np.int32(tr) for tr in self.tracks], False, DEF.GREEN)
                 if (len(self.tracks) > 0):
-                    draw_str(frame_to_draw, (20, 20), 'track count: %d' % len(self.tracks))
-                    draw_str(frame_to_draw, (20, 40), 'mean travel: %f' %np.mean(d))
-                    draw_str(frame_to_draw, (20, 60), 'dX: %f' %d_x_mean)
-                    draw_str(frame_to_draw, (20, 80), 'dY: %f' %d_y_mean)
+                    draw_str(canvas, (20, 20), 'track count: %d' % len(self.tracks))
+                    draw_str(canvas, (20, 40), 'mean travel: %f' %np.mean(d))
+                    draw_str(canvas, (20, 60), 'dX: %f' %d_x_mean)
+                    draw_str(canvas, (20, 80), 'dY: %f' %d_y_mean)
                 else:
-                    cv2.putText(frame_to_draw, "lost track", (20, 30), cv2.FONT_HERSHEY_DUPLEX, 1, DEF.RED)
+                    cv2.putText(canvas, "lost track", (20, 30), cv2.FONT_HERSHEY_DUPLEX, 1, DEF.RED)
             else:
-                frame_to_draw = cv2.addWeighted(frame_to_draw, 0.3, frame_copy, 0.7, 0.0)
+                canvas = cv2.addWeighted(canvas, 0.3, frame_w_edges, 0.7, 0.0)
             if self.frame_idx % self.detect_interval == 0:
                 mask = np.zeros_like(frame_gray)
                 mask[:] = 255
@@ -97,33 +123,46 @@ class App:
                 for x, y in np.float32(p).reshape(-1, 2):
                     self.tracks.append([(x, y)])
 
+    def run(self):
+            frames = []
+            global canvas
+            edges = self.__find_edges(frame)
+            b,g,r = cv2.split(frame)
+            z = np.zeros_like(b)
+            o = np.ones_like(b)*255
+            #frame_w_canvas = cv2.merge((b,g,r,cv2.cvtColor(canvas, cv2.COLOR_BGR2GRAY)))
+            frame_w_canvas = cv2.bitwise_or(frame, canvas)
+            frame_w_edges = cv2.bitwise_and(frame, edges)
             self.frame_idx += 1
             self.prev_gray = frame_gray
-            #cv2.imshow('lk_track', frame_to_draw)
 
-            frames = (frame_to_draw, edges)
+            frames = {'original':frame, 'canvas':frame_w_canvas, 'edges':frame_w_edges}
             return frames
             #return mask
 
+    def show_video(self, video_out):
+        cv2.imshow(self.name, video_out)
+
+    def close(self):
+        capture.release()
+        cv2.destroyAllWindows()
 
 class Object:
     x = 0 #these will hold the coordinates of the object "centers". see moments of inertia
     y = 0
     def __init__(self, name="null"):
         self.type = "Object"
-        self.Color = (0,0,0)
+        self.color = (0,0,0)
         self.position = 0
         self.area = 0
+        if name is "pepper_red":
+         self.color = DEF.RED
         if name is "blue":
-         #self.HSVmin = (92, 0, 0)
-         #self.HSVmax = (124, 256, 256)
-         self.Color = DEF.BLUE
-        if name is "red":
-         #self.HSVmin = (0, 200, 0)
-         #self.HSVmax = (19, 255, 255)
-         self.Color = DEF.RED
+         self.color = DEF.BLUE
 
-def drawObject(theObjects, frame, temp, contours, hierarchy):
+def drawObject(theObjects, frame, contours, hierarchy):
+    print("drawObjects")
+    #sort objects by x coordinate
     theObjects = (sorted(theObjects, key=operator.attrgetter('x')))
 
     while (len(theObjects)>0):
@@ -134,26 +173,16 @@ def drawObject(theObjects, frame, temp, contours, hierarchy):
         #    print("breakpoint")
         cv2.drawContours(frame,contours,thisObject.position, DEF.RED, 5, 5)
         coordinate = (thisObject.x,thisObject.y) #contains coordinates of the object: (int(moment['m10']/area))
-        cv2.circle(frame, coordinate, 10, thisObject.Color) #draw circle at the object center
+        cv2.circle(frame, coordinate, 10, thisObject.color) #draw circle at the object center
         text_org = (thisObject.x+10, thisObject.y)
-        if thisObject.Color == DEF.RED:
+        if thisObject.color == DEF.RED:
             obj_number = str(len(theObjects))
             cv2.putText(frame, obj_number, text_org, cv2.FONT_HERSHEY_DUPLEX, 1, DEF.WHITE)
-        if thisObject.Color == DEF.BLUE:
-            cv2.putText(frame, "Oreos", text_org, cv2.FONT_HERSHEY_DUPLEX, 1.2, thisObject.Color)
+        if thisObject.color == DEF.BLUE:
+            cv2.putText(frame, "Oreos", text_org, cv2.FONT_HERSHEY_DUPLEX, 1.2, thisObject.color)
 
-def morphOps(thresh):
-    #create structuring element that will be used to "dilate" and "erode" image.
-    #the element chosen here is a 3px by 3px rectangle
-    erodeElement = cv2.getStructuringElement( cv2.MORPH_ELLIPSE,(3,3))
-    #dilate with larger element so make sure object is nicely visible
-    dilateElement = cv2.getStructuringElement( cv2.MORPH_ELLIPSE,(8,8))
-    thresh = cv2.erode(thresh,erodeElement,2)
-    thresh = cv2.dilate(thresh,dilateElement,2)
 
-    return thresh
-
-def trackFilteredObject(theObject, threshold, HSV, cameraFeed):
+def trackFilteredObject(theObject, threshold, HSV, thisFrame):
     temp = threshold.copy()
     objects = []
     _, contours, hierarchy = cv2.findContours(temp, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
@@ -173,7 +202,7 @@ def trackFilteredObject(theObject, threshold, HSV, cameraFeed):
                 object.x = int(moment['m10']/area)
                 object.y = int(moment['m01']/area)
                 object.type = theObject.type
-                object.Color = theObject.Color
+                object.color = theObject.color
                 object.position = i
                 objects.append(object)
                 #print len(objects)
@@ -185,99 +214,63 @@ def trackFilteredObject(theObject, threshold, HSV, cameraFeed):
         "objects: %d, contours: %d" % (len(objects), len(contours))
 
     if objectFound is True:
-            drawObject(objects,cameraFeed,temp,contours,hierarchy)
+            drawObject(objects,thisFrame,contours,hierarchy)
             #print "supposed to draw"
 
 
-# global variables #
-cap = cv2.VideoCapture("./p4.mp4") #putting '0' tries to load the default cam on the machine
-opflow = App("0")
+APP = App("Gothue tracking", "./p2.mp4")
 
-# main #
+
 def main():
-  cv2.namedWindow('object_discovered', cv2.WINDOW_NORMAL)
-  cv2.resizeWindow('object_discovered', 1920/2,1080/2)
-  color_filters.color_filter_init()
-  mode = '0'
-  paused = False
-  while (cap.isOpened()):
-    if paused == False:
-        _, frame = cap.read()
-        orig_frame = frame.copy()
-        # Capture frame-by-frame
-        #_, frame = cap.read()
-        blue = Object("blue")
-        red = Object("red")
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('frame', 1920/2,1080/2)
+    mode = '0'
+    paused = False
+    red_filter = FT.ColorFilter("red", FT.filter_red)
+    while APP.video_status():
+        gothue = Object("pepper_red")
+
+        frames = APP.run()
+        APP.next_frame()
+        hsv, red_threshold = red_filter.apply_filter(frames['original'])
+
+        trackFilteredObject(gothue, red_threshold, hsv, frames['canvas'])
 
 
-        #add color ranges as a weighted sum (tones of the same color) (i.e. crimson vs slight orange tint)
-        lower_red_hue_range = cv2.inRange(hsv, np.array(color_filters.red.filter_settings['lower']['lowb']), np.array(color_filters.red.filter_settings['lower']['upb']))
-        upper_red_hue_range = cv2.inRange(hsv, np.array(color_filters.red.filter_settings['upper']['lowb']), np.array(color_filters.red.filter_settings['upper']['upb']))
-        threshold_red = cv2.addWeighted(lower_red_hue_range, float(color_filters.red.filter_settings['weight']), upper_red_hue_range, float(1.0-color_filters.red.filter_settings['weight']), 0.0)
-        threshold_red = cv2.GaussianBlur(threshold_red, (5,5), 0 )
-        #morphoOps erodes & dilates sequentially to "open" the previously filtered hsv image
-        threshold_red_opened = morphOps(threshold_red)
-        threshold_red_opened = cv2.threshold(threshold_red_opened, 127, 255, cv2.THRESH_BINARY)[1]
-        trackFilteredObject(red, threshold_red_opened, hsv, frame)
-
-        lower_blue_hue_range = cv2.inRange(hsv, np.array(color_filters.blue.filter_settings['lower']['lowb']), np.array(color_filters.blue.filter_settings['lower']['upb']))
-        upper_blue_hue_range = cv2.inRange(hsv, np.array(color_filters.blue.filter_settings['upper']['lowb']), np.array(color_filters.blue.filter_settings['upper']['upb']))
-        threshold_blue = cv2.addWeighted(lower_blue_hue_range, float(color_filters.blue.filter_settings['weight']), upper_blue_hue_range, float(1.0-color_filters.blue.filter_settings['weight']), 0.0)
-        threshold_blue_opened = morphOps(threshold_blue)
-        trackFilteredObject(blue, threshold_blue_opened, hsv, frame)
-
-        #split channels then add filtered threshold (binary image) to the original
-        #this is done so that we can see how well the filter is working
-        b,g,r = cv2.split(frame)
-        b = cv2.addWeighted(threshold_red_opened, 0.5, b, 0.5, 0.1)
-        g = cv2.addWeighted(threshold_red_opened, 0.5, g, 0.5, 0.1)
-        r = cv2.addWeighted(threshold_red_opened, 0.5, r, 0.5, 0.1)
-        frame = cv2.merge((b,g,r))
-
-        frames = opflow.run(orig_frame, frame, threshold_red_opened)
-        #cv2.add(vis, frame, frame)
-        #cv2.imshow('object_discovered', frame)
-
-    if mode is '0':
-        cv2.imshow('frame',frames[0])
-    if mode is '1':
-        cv2.imshow('frame', frames[1])
-    if mode is '2':
-        cv2.imshow('frame', frame)
-    if mode is '3':
-        cv2.imshow('frame', threshold_red_opened)
-    if mode is '4':
-        cv2.imshow('frame', orig_frame)
-
-    key_pressed = cv2.waitKey(1) & 0xFF
-
-    #press m to change display mode
-    if key_pressed == ord('m'):
         if mode is '0':
-            mode = '1'
-        elif mode is '1':
-            mode = '2'
-        elif mode is '2':
-            mode = '3'
-        elif mode is '3':
-            mode = '4'
-        elif mode is '4':
-            mode = '0'
-    #press p for pause
-    if key_pressed == ord('p'):
-        paused = not(paused)
-    #press q to close the program
-    if key_pressed == ord('q'):
-        break
-    #press t to enable trackbars for filters (not working yet)
-    if key_pressed == ord('t'):
-        print "trackbar"
-        color_filters.open_trackbar()
+            APP.show_video(frames['original'])
+        if mode is '1':
+            APP.show_video(frames['canvas'])
+        if mode is '2':
+            APP.show_video(frames['edges'])
+        if mode is '3':
+            cv2.imshow(APP.name, cv2.bitwise_and(frames['original'],cv2.cvtColor(red_threshold, cv2.COLOR_GRAY2BGR)))
 
-  # When everything done, release the capture
-  cap.release()
-  cv2.destroyAllWindows()
+        key_pressed = cv2.waitKey(1) & 0xFF
+
+        #press m to change display mode
+        if key_pressed == ord('m'):
+            if mode is '0':
+                mode = '1'
+            elif mode is '1':
+                mode = '2'
+            elif mode is '2':
+                mode = '3'
+            elif mode is '3':
+                mode = '0'
+
+        #press p for pause
+        if key_pressed == ord('p'):
+            paused = not(paused)
+        #press q to close the program
+        if key_pressed == ord('q'):
+            break
+        #press t to enable trackbars for filters (not working yet)
+        if key_pressed == ord('t'):
+            print "trackbar"
+            FT.open_trackbar()
+    # When everything done, release the capture
+    APP.close()
 
 if __name__ == "__main__":
     main()
