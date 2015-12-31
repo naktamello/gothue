a9e5d9667263718dda2640935391a30fd9cd94aa
naktamello <joejsy@gmail.com>
2015-12-31 14:09:33 +0900

    added gauge needle orientation detection

--- defines.py
+++ defines.py
@@ -6,3 +6,7 @@ RED = (0, 0, 255)
 WHITE = (255, 255, 255)
 YELLOW = (52, 255, 255)
 AREA = 3000
+
+#(pressure in Mpa, angle in degrees)
+GAUGE_MIN = {'pressure':0.0, 'angle':33.0}
+GAUGE_MAX = {'pressure':3.5, 'angle':327.0}
--- gauge_det.py
+++ gauge_det.py
@@ -1,34 +1,40 @@
 #!/usr/bin/python2.7
 import sys
+import math
 import numpy as np
 import cv2
 import defines as DEF
 
+
 def make_line(p1, p2):
     A = (p1[1] - p2[1])
     B = (p2[0] - p1[0])
-    C = (p1[0]*p2[1] - p2[0]*p1[1])
+    C = (p1[0] * p2[1] - p2[0] * p1[1])
     return A, B, -C
 
+
 def intersection(L1, L2):
-    D  = L1[0] * L2[1] - L1[1] * L2[0]
+    D = L1[0] * L2[1] - L1[1] * L2[0]
     Dx = L1[2] * L2[1] - L1[1] * L2[2]
     Dy = L1[0] * L2[2] - L1[2] * L2[0]
     if D != 0:
         x = Dx / D
         y = Dy / D
-        return x,y
+        return x, y
     else:
         return False
 
+
 class Object:
-    x = 0 #these will hold the coordinates of the object "centers". see moments of inertia
+    x = 0  # these will hold the coordinates of the object "centers". see moments of inertia
     y = 0
+
     def __init__(self, name="null"):
         self.type = "Object"
         self.color = DEF.RED
         self.position = 0
         self.area = 0
+
 
 def main():
     # parameters
@@ -64,8 +70,8 @@ def main():
     if do_threshold:
         # create structuring element that will be used to "dilate" and "erode" image.
         # the element chosen here is a 3px by 3px rectangle
-        prep = cv2.adaptiveThreshold(prep, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV,25,3)
-        #prep = cv2.threshold(prep, 50, 255, cv2.THRESH_BINARY_INV)[1]
+        prep = cv2.adaptiveThreshold(prep, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 25, 3)
+        # prep = cv2.threshold(prep, 50, 255, cv2.THRESH_BINARY_INV)[1]
         erodeElement = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
         # dilate with larger element so make sure object is nicely visible
         dilateElement = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
@@ -75,8 +81,9 @@ def main():
     # Hough transforms (circles, lines, plines)
     if hough_circle:
         # HoughCircles returns output vector (x,y,radius) inside an one element list so [0] is appended at the end
-        circles = cv2.HoughCircles(gray_im, cv2.HOUGH_GRADIENT, 2, 10, None, param1=50, param2=30, minRadius=height / 10,
-                                   maxRadius=0, )[0]
+        circles = \
+        cv2.HoughCircles(gray_im, cv2.HOUGH_GRADIENT, 2, 10, None, param1=50, param2=30, minRadius=height / 10,
+                         maxRadius=0, )[0]
         if circles is None:
             sys.exit("could not locate the gauge circle")  # if no circles are found terminate the program here
         else:
@@ -94,41 +101,40 @@ def main():
         _, contours, hierarchy = cv2.findContours(copy, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         if contours is not None and len(contours) > 0:
             numObjects = len(contours)
-            #print("objects = %d" % numObjects )
-            if numObjects<1024:
-                for i in range (0, len(contours)):
-                  moment = cv2.moments(contours[i])
-                  area = moment['m00']
-                  if area > DEF.AREA:
-                    #print("area = %d" % area)
-                    object = Object()
-                    object.area = area
-                    object.x = int(moment['m10']/area)
-                    object.y = int(moment['m01']/area)
-                    object.position = i
-                    objects.append(object)
-                    #print len(objects)
-                    #print "area is greater than .."
-                    objectFound = True
-                  #else: objectFound = False
+            # print("objects = %d" % numObjects )
+            if numObjects < 1024:
+                for i in range(0, len(contours)):
+                    moment = cv2.moments(contours[i])
+                    area = moment['m00']
+                    if area > DEF.AREA:
+                        # print("area = %d" % area)
+                        object = Object()
+                        object.area = area
+                        object.x = int(moment['m10'] / area)
+                        object.y = int(moment['m01'] / area)
+                        object.position = i
+                        objects.append(object)
+                        # print len(objects)
+                        # print "area is greater than .."
+                        objectFound = True
+                        # else: objectFound = False
 
-        assert(len(objects) <= len(contours)), \
+        assert (len(objects) <= len(contours)), \
             "objects: %d, contours: %d" % (len(objects), len(contours))
-        while (len(objects)>0):
+        while (len(objects) > 0):
             thisObject = objects.pop()
-            #contours[thisObject.position] = cv2.convexHull(contours[thisObject.position], returnPoints = True)
-            cv2.drawContours(blank,contours,thisObject.position, DEF.WHITE, 5, 2)
-
+            # contours[thisObject.position] = cv2.convexHull(contours[thisObject.position], returnPoints = True)
+            cv2.drawContours(blank, contours, thisObject.position, DEF.WHITE, 5, 2)
 
     if hough_line:
         blank = cv2.cvtColor(blank, cv2.COLOR_BGR2GRAY)
         lines = cv2.HoughLines(blank, 1, np.pi / 90, 50)
     if hough_pline:
-        plines = cv2.HoughLinesP(prep, 1, np.pi / 180, 20, None, minLineLength = height/10, maxLineGap = 1)
+        plines = cv2.HoughLinesP(prep, 1, np.pi / 180, 20, None, minLineLength=height / 10, maxLineGap=1)
 
     first = True
     for c in circles[:1]:
-        # green for circles (only draw the 3 strongest)
+        # green for circles (only draw the 1 strongest)
         cv2.circle(canvas, (c[0], c[1]), c[2], (0, 255, 0), line_thickness)
         # cv2.circle()
         # res = cv2.bitwise_and(frame,frame, mask= mask)
@@ -137,73 +143,53 @@ def main():
             circle_x = c[0]
             circle_y = c[1]
             radius = int(c[2])
-    first = True
+
     needle_lines = []
     angles = []
     for line in lines[:2]:
         for (rho, theta) in line:
-            # blue for infinite lines (only draw the 5 strongest)
+            # blue for infinite lines (only draw the 2 strongest)
             x0 = np.cos(theta) * rho
             y0 = np.sin(theta) * rho
             pt1 = (int(x0 + (height + width) * (-np.sin(theta))), int(y0 + (height + width) * np.cos(theta)))
             pt2 = (int(x0 - (height + width) * (-np.sin(theta))), int(y0 - (height + width) * np.cos(theta)))
             cv2.line(canvas, pt1, pt2, (255, 0, 0), line_thickness)
-            needle_lines.append(make_line(pt1,pt2))
+            needle_lines.append(make_line(pt1, pt2))
             angles.append(theta)
-            i += 1
-            if first:
-                first = False
-                line_x = x0
-                line_y = y0
-                #angle = 90 - (theta * 360 / (2 * np.pi))
-                line_mask = np.zeros_like(mask)
-                rect_height = int(radius / 4)
-                rect_width = int(radius * 2)
-                cv2.line(line_mask, pt1, pt2, (255, 255, 255),
-                         rect_height)  # mask width is quarter of circle radius found above
-                mask = cv2.bitwise_and(mask, line_mask)
-                M = cv2.getRotationMatrix2D((x0, y0), (theta * 360 / (2 * np.pi)) - 90, 1)
-    avg_theta = (angles[0] + angles[1])/2
-    angle = ((avg_theta * 360 / (2 * np.pi)))
-    if angle > 360.0:
-        angle -= 360
-    assert(len(needle_lines) >= 2)
+
+    avg_theta = (angles[0] + angles[1]) / 2
+    assert (len(needle_lines) >= 2)
     R = intersection(needle_lines[0], needle_lines[1])
     if R:
         print "Intersection detected:", R
         cv2.circle(canvas, R, 10, DEF.RED, 3)
     else:
         print "No single intersection point detected"
-    prep = cv2.bitwise_and(prep, mask)
-    density = cv2.warpAffine(prep, M, (width, height))
-    density_rect_width = radius * 2
-    if density_rect_width % 2 is not 0:
-        density_rect_width -= 1
-    density_rect_height = rect_height / 3
-    if density_rect_height % 2 is not 0:
-        density_rect_height -= 1
-    density_rect_origin_x = int(line_x - density_rect_width / 2)
-    density_rect_origin_y = int(line_y - density_rect_height / 2)
-    density_rect = np.zeros(shape=(density_rect_height, density_rect_width)).astype(np.uint8)
-    for i, row in enumerate(density_rect):
-        for j, pixel in enumerate(row):
-            density_rect[i, j] = density[density_rect_origin_y + i, density_rect_origin_x + j]
-    left, right = np.hsplit(density_rect, 2)
-    left_density = 0
-    right_density = 0
-    for row in left:
-        for pixel in row:
-            if pixel:
-                left_density += 1
-    for row in right:
-        for pixel in row:
-            if pixel:
-                right_density += 1
-    if left_density > right_density:
-        pass
-        #angle += 180
+    guess1 = [avg_theta, (0, 0)]
+    guess2 = [avg_theta + np.pi, (0, 0)]
+    if guess2[0] > 2 * np.pi:
+        guess2[0] -= 2 * np.pi
+    guess1[1] = (int(circle_x + radius * np.sin(guess1[0])), int(circle_y - radius * np.cos(guess1[0])))
 
-    cv2.putText(canvas, "angle:" + str(angle), (int(height * 0.4), int(width * 0.6)), cv2.FONT_HERSHEY_PLAIN, 2, (200, 50, 0), 3)
+    avg_theta += np.pi
+    guess2[1] = (int(circle_x + radius * np.sin(guess2[0])), int(circle_y - radius * np.cos(guess2[0])))
+
+    dist1 = math.hypot(R[0] - guess1[1][0], R[1] - guess1[1][1])
+    dist2 = math.hypot(R[0] - guess2[1][0], R[1] - guess2[1][1])
+    if dist1 < dist2:
+        angle = (guess1[0] * 360 / (2 * np.pi))
+    else:
+        angle = (guess2[0] * 360 / (2 * np.pi))
+    angle += 180
+    if angle >= 360:
+        angle -= 360
+
+    pressure = np.interp(angle,[DEF.GAUGE_MIN['angle'],DEF.GAUGE_MAX['angle']],[DEF.GAUGE_MIN['pressure'],DEF.GAUGE_MAX['pressure']])
+    print "pressure = ", pressure
+    prep = cv2.bitwise_and(prep, mask)
+
+    cv2.putText(canvas, "pressure:" + str(round(pressure,3)) + " MPa", (int(height * 0.4), int(width * 0.6)), cv2.FONT_HERSHEY_PLAIN,
+                fontScale=(height / 200), color=(200, 50, 0), thickness=3)
 
     if hough_pline:
         for pline in plines[:10]:
